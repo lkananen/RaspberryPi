d0bbebe9b99740d4637cc6bcf990ea76112f2b40
lkananen <lauri.kananen@gmail.com>
2019-06-02 14:07:46 +0300

    XFCE installer script created

---
+++ init/install_xfce.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script installs XFCE desktop using the instructions from:
+# https://wiki.debian.org/Xfce
+#
+sudo apt-get install xfce4 -y
+sudo apt-get install xfce4-goodies -y
