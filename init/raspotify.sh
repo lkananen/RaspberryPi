a8508bee564994821c105a9183d03a5537cc5060
lkananen <lauri.kananen@gmail.com>
2019-06-02 17:56:01 +0300

    Bluetooth settings for Raspotify.

--- init/raspotify.sh
+++ init/raspotify.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-# Source: https://github.com/dtcooper/raspotify
-# Sets your Raspberry Pi to be a Spofify device
+# Turns Raspberry Pi into a spotify device.
+# Sources:
+# https://github.com/dtcooper/raspotify
+# https://wiki.debian.org/BluetoothUser/a2dp
+#
+# Sets up the Spofify device:
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+#
+# Modifies Bluetooth settings, so you can connect a wireless speaker:
+apt-get install pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
+#
+# Restart the services:
+service bluetooth restart
+killall pulseaudio
+#
+# Install Bluetooth GUI pairing tool:
+apt-get install blueman
