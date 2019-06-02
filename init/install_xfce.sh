e8110e57c537a331a038f0432a2a0a061c4a92ca
lkananen <lauri.kananen@gmail.com>
2019-06-02 15:16:21 +0300

    XFCE installer dependency fix

--- init/install_xfce.sh
+++ init/install_xfce.sh
@@ -2,5 +2,10 @@
 # This script installs XFCE desktop using the instructions from:
 # https://wiki.debian.org/Xfce
 #
+sudo aptitude purge `dpkg --get-selections | grep gnome | cut -f 1` -y
+sudo aptitude -f install -y
+sudo aptitude purge `dpkg --get-selections | grep deinstall | cut -f 1` -y
+sudo aptitude -f install -y
+#
 sudo apt-get install xfce4 -y
 sudo apt-get install xfce4-goodies -y
