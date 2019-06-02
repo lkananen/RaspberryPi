9f7901ffa715733f5bb6fb68f56e08d15d591dd7
lkananen <lauri.kananen@gmail.com>
2019-06-02 17:02:55 +0300

    Added raspotify to init.sh

--- init/init.sh
+++ init/init.sh
@@ -2,5 +2,12 @@
 # This file runs all the setup scripts and initializes the system.
 # Can be run on a fresh install of Raspian.
 #
+# Rigths to run the scripts:
+chmod +x ./filesystem_upgrade.sh
+chmod +x ./basic_apps.sh
+chmod +x ./raspotify.sh
+#
+# Script executions:
 ./filesystem_upgrade.sh
 ./basic_apps.sh
+./raspotify.sh
