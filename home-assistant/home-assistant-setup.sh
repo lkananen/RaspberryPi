5653b4823742fdeff5735512ab42cfd9c367c91a
Lauri Kananen <lauri.kananen@gmail.com>
2022-07-10 22:06:42 +0300

    Create home-assistant-setup.sh as empty

---
+++ home-assistant/home-assistant-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Sets up Home Assistant in a Docker container.
+# Requires Docker setup!
+#
+# More instructions at:
+# https://www.home-assistant.io/installation/raspberrypi#install-home-assistant-container
+#
+###############################################################################
+# TODO
