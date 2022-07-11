aaa1693259e4899b24ba1b26762fa18c22f94086
Lauri Kananen <lauri.kananen@gmail.com>
2022-07-11 14:54:05 +0300

    Device integration instructions added.

--- home-assistant/z-wave-setup.sh
+++ home-assistant/z-wave-setup.sh
@@ -4,6 +4,7 @@
 # After this the device is ready to be used with other systems like Home Assistant.
 #
 # Requires the device to be connected to a USB port.
+# More details: https://www.home-assistant.io/installation/raspberrypi#exposing-devices
 #
 ###############################################################################
 # Device info
@@ -11,3 +12,9 @@
 # Obtain Aeotec Z-stick USB ACM device.
 # Usually named "ttyACM0".
 dmesg | grep tty
+
+# Add that device to the Docker commands:
+# A) `docker run ... --device /dev/ttyUSB0:/dev/ttyUSB0 ...`
+# B) Compose file:
+# ```    devices:
+#         - /dev/ttyUSB0:/dev/ttyUSB0.```
