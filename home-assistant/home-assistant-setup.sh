a4611eccc83d140a516364e7f8c9d4bb84c196d9
Lauri Kananen <lauri.kananen@gmail.com>
2022-07-11 12:38:49 +0300

    Docker container update instructions.

--- home-assistant/home-assistant-setup.sh
+++ home-assistant/home-assistant-setup.sh
@@ -19,7 +19,28 @@ docker run -d \
   -v /home/pi/ha:/config \
   --network=host \
   ghcr.io/home-assistant/home-assistant:stable
-  
+
+
+###############################################################################
+# Updating
+
+# Check for newest image.
+# If using the latest already, update process can be stopped here.
+docker pull ghcr.io/home-assistant/home-assistant:stable
+
+# If new one is found, remove old one.
+docker stop homeassistant
+docker rm homeassistant
+
+# Re-starting with newer image.
+docker run -d \
+  --name homeassistant \
+  --privileged \
+  --restart=unless-stopped \
+  -e TZ=Europe/Helsinki \
+  -v /home/pi/ha:/config \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
 
 ###############################################################################
 # Restarting
