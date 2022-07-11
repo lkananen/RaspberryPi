5f74ff7f2fc1820d08ec957aefec782c2ae9d803
Lauri Kananen <lauri.kananen@gmail.com>
2022-07-11 21:50:04 +0300

    Bluetooth disable on-board command added.

--- home-assistant/z-wave-setup.sh
+++ home-assistant/z-wave-setup.sh
@@ -18,3 +18,17 @@ dmesg | grep tty
 # B) Compose file:
 # ```    devices:
 #         - /dev/ttyUSB0:/dev/ttyUSB0.```
+
+
+###############################################################################
+# Device settings
+
+# Bluetooth needs to be disable on-board to use hardware UART.
+# Following appends disable command to boot config.
+echo "dtoverlay=disable-bt" | tee -a /boot/config.txt 
+
+# Non-Raspberry Pi OS needs also following command to be run:
+#sudo systemctl disable hciuart
+
+# Note! Reboot required!
+#reboot
