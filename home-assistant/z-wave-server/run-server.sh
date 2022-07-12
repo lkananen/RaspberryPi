bb1b69285200eb52b21929215a8bcd9bde684947
Lauri Kananen <lauri.kananen@gmail.com>
2022-07-12 12:42:04 +0300

    Z-wave server docker-compose setup added.

--- home-assistant/z-wave-server/run-server.sh
+++ home-assistant/z-wave-server/run-server.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Downloads the server repository if the repo folder does not exist
-if [ -d "./zwave-js-server" ]
-then
-  echo "Starting the server in /zwave-js-server/src/bin/server.ts"
-else
-  echo "Downloading server wrapper..."
-  git clone https://github.com/zwave-js/zwave-js-server.git
-  echo "Running the server..."
-fi
-
-echo "Server starting on 'ws://0.0.0.0:3000'"
-cd zwave-js-server
-ts-node src/bin/server.ts /dev/tty0
+mkdir ~/ha-cache
+docker-compose -f docker-compose.yaml up -d
